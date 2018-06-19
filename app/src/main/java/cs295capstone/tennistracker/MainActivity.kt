df13b15b663f224da21440fd4903039d2e0ad99b
icygub <jchubbs95@gmail.com>
2018-06-19 15:55:11 -0600

    starting new match setup layout

--- app/src/main/java/cs295capstone/tennistracker/MainActivity.kt
+++ app/src/main/java/cs295capstone/tennistracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package cs295capstone.tennistracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -8,8 +9,10 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -29,6 +32,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        button_new_match.setOnClickListener{
+            val intent = Intent(this, NewMatchSetupActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onBackPressed() {
---
+++ app/src/main/java/cs295capstone/tennistracker/NewMatchSetupActivity.kt
@@ -0,0 +1,12 @@
+package cs295capstone.tennistracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewMatchSetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_match_setup)
+    }
+}
