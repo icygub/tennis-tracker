60188d628ceb4b6946e5a9b136c358bfe4d50061
icygub <jchubbs95@gmail.com>
2018-06-19 21:25:17 -0600

    serve outcome page

--- app/src/main/java/cs295capstone/tennistracker/MatchMainPageActivity.kt
+++ app/src/main/java/cs295capstone/tennistracker/MatchMainPageActivity.kt
@@ -1,12 +1,19 @@
 package cs295capstone.tennistracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.match_main_page.*
 
 class MatchMainPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.match_main_page)
+
+        button_start_point.setOnClickListener {
+            val intent = Intent(this, ServeOutcomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/cs295capstone/tennistracker/ServeOutcomeActivity.kt
@@ -0,0 +1,12 @@
+package cs295capstone.tennistracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ServeOutcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.serve_outcome)
+    }
+}
