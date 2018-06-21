21d01033ff528ef0b0293bff171b2acf6f0c0e7a
icygub <jchubbs95@gmail.com>
2018-06-21 15:39:50 -0600

    started point outcome ui

--- app/src/main/java/cs295capstone/tennistracker/ServeOutcomeActivity.kt
+++ app/src/main/java/cs295capstone/tennistracker/ServeOutcomeActivity.kt
@@ -1,12 +1,24 @@
 package cs295capstone.tennistracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.serve_outcome.*
 
 class ServeOutcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.serve_outcome)
+
+        button_ace.setOnClickListener {
+            val intent = Intent(this, MatchMainPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_continue.setOnClickListener {
+            val intent = Intent(this, PointOutcomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
